7d02216adc4be78413c914b07df5681d4b8536eb
KosMicBor <gaijin-dk@mail.ru>
2022-09-02 20:08:05 +0500

    Добавил методы отображения состояний для экранов

--- app/src/main/java/gu_android_team/modernbooklibrary/ui/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/gu_android_team/modernbooklibrary/ui/mainscreen/MainScreenFragment.kt
@@ -1,17 +1,48 @@
 package gu_android_team.modernbooklibrary.ui.mainscreen
 
-import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import gu_android_team.modernbooklibrary.R
+import gu_android_team.modernbooklibrary.databinding.FragmentMainScreenBinding
+import gu_android_team.modernbooklibrary.domain.Screen
 
-class MainScreenFragment : Fragment(R.layout.fragment_main_screen) {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+class MainScreenFragment : Fragment(R.layout.fragment_main_screen), Screen {
 
     companion object {
 
         fun newInstance() = MainScreenFragment()
+    }
+
+    private val binding: FragmentMainScreenBinding by viewBinding(
+        FragmentMainScreenBinding::bind
+    )
+
+    override fun showProgress() {
+        with(binding) {
+            mainProgressBar.visibility = View.VISIBLE
+            mainContainerNestedScrollView.visibility = View.GONE
+        }
+    }
+
+    override fun showStandardScreen() {
+        with(binding) {
+            mainProgressBar.visibility = View.GONE
+            mainContainerNestedScrollView.visibility = View.VISIBLE
+        }
+    }
+
+    override fun showError(message: String) {
+
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.retry_string) {
+                updateData()
+            }
+            .show()
+    }
+
+    private fun updateData() {
+        TODO("Not yet implemented")
     }
 }
