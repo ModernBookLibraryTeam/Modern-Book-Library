01fc47e1ae89fe135ddfb6f74307ca375803d6f4
KosMicBor <gaijin-dk@mail.ru>
2022-09-02 20:08:05 +0500

    Добавил методы отображения состояний для экранов

--- app/src/main/java/gu_android_team/modernbooklibrary/ui/favoritesscreen/FavoritesScreenFragment.kt
+++ app/src/main/java/gu_android_team/modernbooklibrary/ui/favoritesscreen/FavoritesScreenFragment.kt
@@ -1,23 +1,51 @@
 package gu_android_team.modernbooklibrary.ui.favoritesscreen
 
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import gu_android_team.modernbooklibrary.R
+import gu_android_team.modernbooklibrary.databinding.FragmentFavoritesScreenBinding
+import gu_android_team.modernbooklibrary.domain.Screen
 
-class FavoritesScreenFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_favorites_screen, container, false)
-    }
+class FavoritesScreenFragment : Fragment(R.layout.fragment_favorites_screen), Screen {
 
     companion object {
         @JvmStatic
         fun newInstance() = FavoritesScreenFragment()
+    }
+
+    private val binding: FragmentFavoritesScreenBinding by viewBinding(
+        FragmentFavoritesScreenBinding::bind
+    )
+
+    override fun showProgress() {
+        with(binding) {
+            favoritesProgressBar.visibility = View.VISIBLE
+            favoritesEmptyImageView.visibility = View.GONE
+            favoritesFilterFab.visibility = View.GONE
+            favoritesListRecyclerView.visibility = View.GONE
+        }
+    }
+
+    override fun showStandardScreen() {
+        with(binding) {
+            favoritesProgressBar.visibility = View.GONE
+            favoritesEmptyImageView.visibility = View.VISIBLE
+            favoritesFilterFab.visibility = View.VISIBLE
+            favoritesListRecyclerView.visibility = View.VISIBLE
+        }
+    }
+
+    override fun showError(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.retry_string) {
+                getFavoritesList()
+            }
+            .show()
+    }
+
+    private fun getFavoritesList() {
+        TODO("Not yet implemented")
     }
 }
