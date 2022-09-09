cf0c3cdb96708627fe59f7781354314193d090de
KosMicBor <gaijin-dk@mail.ru>
2022-09-09 21:23:19 +0500

    Рефакторинг навигации с использованием Navigation UI.

--- app/src/main/java/gu_android_team/modernbooklibrary/MainActivity.kt
+++ app/src/main/java/gu_android_team/modernbooklibrary/MainActivity.kt
@@ -5,15 +5,14 @@ import android.os.Bundle
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.textfield.TextInputEditText
 import gu_android_team.modernbooklibrary.databinding.ActivityMainBinding
-import gu_android_team.modernbooklibrary.ui.bookdescriptionscreen.BookDescriptionFragment
-import gu_android_team.modernbooklibrary.ui.favoritesscreen.FavoritesScreenFragment
 import gu_android_team.modernbooklibrary.ui.mainscreen.MainScreenFragment
-import gu_android_team.modernbooklibrary.ui.profilescreen.ProfileScreenFragment
-import gu_android_team.modernbooklibrary.ui.searchscreen.SearchScreenFragment
 
 class MainActivity : AppCompatActivity(), MainScreenFragment.MainScreenController {
 
@@ -26,20 +25,18 @@ class MainActivity : AppCompatActivity(), MainScreenFragment.MainScreenControlle
         const val APP_SHARED_PREFERENCES = "APP_SHARED_PREFERENCES"
     }
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .setReorderingAllowed(true)
-                .replace(R.id.mainContainerFrameLayout, MainScreenFragment.newInstance())
-                .commit()
-        }
-
         bottomNavigation = binding.bottomNavigationView
 
-        initBottomMenu()
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainContainerFrameLayout) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(bottomNavigation, navController)
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -59,65 +56,8 @@ class MainActivity : AppCompatActivity(), MainScreenFragment.MainScreenControlle
         return super.dispatchTouchEvent(ev)
     }
 
-    private fun initBottomMenu() {
-
-        bottomNavigation.setOnItemSelectedListener {
-
-            when (it.itemId) {
-                R.id.bottomMenuMainScreenButton -> {
-                    supportFragmentManager.beginTransaction()
-                        .setReorderingAllowed(true)
-                        .replace(R.id.mainContainerFrameLayout, MainScreenFragment.newInstance())
-                        .addToBackStack("mainScreen")
-                        .commit()
-
-                    true
-                }
-                R.id.bottomMenuSearchScreenButton -> {
-                    supportFragmentManager.beginTransaction()
-                        .setReorderingAllowed(true)
-                        .replace(R.id.mainContainerFrameLayout, SearchScreenFragment.newInstance())
-                        .addToBackStack("searchScreen")
-                        .commit()
-
-                    true
-                }
-
-                R.id.bottomMenuFavoritesScreenButton -> {
-                    supportFragmentManager.beginTransaction()
-                        .setReorderingAllowed(true)
-                        .replace(
-                            R.id.mainContainerFrameLayout,
-                            FavoritesScreenFragment.newInstance()
-                        )
-                        .addToBackStack("favoritesScreen")
-                        .commit()
-
-                    true
-                }
-
-                R.id.bottomMenuProfileScreenButton -> {
-                    supportFragmentManager.beginTransaction()
-                        .setReorderingAllowed(true)
-                        .replace(R.id.mainContainerFrameLayout, ProfileScreenFragment.newInstance())
-                        .addToBackStack("profileScreen")
-                        .commit()
-
-                    true
-                }
-
-                else -> {
-                    false
-                }
-            }
-        }
-    }
-
     override fun openBookDescriptionScreen(bundle: Bundle) {
-        supportFragmentManager.beginTransaction()
-            .setReorderingAllowed(true)
-            .replace(R.id.mainContainerFrameLayout, BookDescriptionFragment.newInstance(bundle))
-            .addToBackStack("bookDescriptionScreen")
-            .commit()
+
+        navController.navigate(R.id.bookDescriptionScreen, bundle)
     }
 }
