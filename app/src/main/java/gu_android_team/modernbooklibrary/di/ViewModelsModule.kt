bd7d9f978eaf1f24ea982ceae4a7c35407f4f495
KosMicBor <gaijin-dk@mail.ru>
2022-09-23 16:18:35 +0500

    Добавил авторизацию в Firebase через Google Аккаунт

--- app/src/main/java/gu_android_team/modernbooklibrary/di/ViewModelsModule.kt
+++ app/src/main/java/gu_android_team/modernbooklibrary/di/ViewModelsModule.kt
@@ -3,11 +3,14 @@ package gu_android_team.modernbooklibrary.di
 import gu_android_team.modernbooklibrary.ui.bookdescriptionscreen.BookDescriptionScreenViewModel
 import gu_android_team.modernbooklibrary.ui.favoritesscreen.FavoritesScreenViewModel
 import gu_android_team.modernbooklibrary.ui.mainscreen.MainScreenViewModel
+import gu_android_team.modernbooklibrary.ui.profilescreen.ProfileScreenViewModel
 import gu_android_team.modernbooklibrary.ui.searchscreen.SearchViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
 const val SEARCH_SCREEN_VIEW_MODEL = "SearchScreenViewModel"
+const val PROFILE_SCREEN_VIEW_MODEL = "ProfileScreenViewModel"
 val viewModelsModule = module {
     viewModel {
         MainScreenViewModel(
@@ -32,5 +35,9 @@ val viewModelsModule = module {
         SearchViewModel(
             usecase = get(named(SEARCH_SCREEN_USECASE_NAME))
         )
+    }
+
+    viewModel(qualifier = named(PROFILE_SCREEN_VIEW_MODEL)) {
+        ProfileScreenViewModel()
     }
 }
--- app/src/main/java/gu_android_team/modernbooklibrary/utils/Utils.kt
+++ app/src/main/java/gu_android_team/modernbooklibrary/utils/Utils.kt
@@ -2,3 +2,4 @@ package gu_android_team.modernbooklibrary.utils
 
 const val ZERO_VAL = 0
 const val ONE_VALUE = 1
+const val DEBUG_TAG = "@@@"
